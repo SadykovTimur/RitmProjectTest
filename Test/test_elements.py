6cf0061383865b462d048545633a082911ba1693
ti.sadykov <Sady-timur@mail.ru>
2024-02-27 15:46:32 +0300

    Added test_elements.py

---
+++ Test/test_elements.py
@@ -0,0 +1,10 @@
+from Page.elements_page import ElementsPage
+from conftest import driver
+
+class TestElements():
+    def test_elements(self,driver):
+        page = ElementsPage(driver, "https://demoqa.com/")
+        page.open()
+        page.click_elements()
+        check_text = page.check_element()
+        assert check_text == "wordFile"
