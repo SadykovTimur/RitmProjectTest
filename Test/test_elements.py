18d21b1b773656163a45abf39c4872880bff2ce8
ti.sadykov <Sady-timur@mail.ru>
2024-02-27 18:56:36 +0300

    Added test_elements.py

--- Test/test_elements.py
+++ Test/test_elements.py
@@ -1,17 +1,13 @@
-import allure
 from Page.elements_page import ElementsPage
 from conftest import driver
 
 
-@allure.feature('TestElements')
 class TestElements:
-    @allure.title('test_elements')
+
     def test_elements(self, driver):
-        with allure.step('Open web site'):
-            page = ElementsPage(driver, "https://demoqa.com/")
+        page = ElementsPage(driver, "https://demoqa.com/")
         page.open()
         page.click_elements()
         check_text = page.check_element()
-        with allure.step('Checking'):
-            assert check_text == "wordFile"
-            print("Test successful")
+        assert check_text == "wordFile"
+        print("Test successful")
